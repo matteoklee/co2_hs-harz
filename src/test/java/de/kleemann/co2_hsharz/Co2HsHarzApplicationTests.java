5023da95ec3fbcb77f4d1a0095434863d0d552e6
matteoklee <117347339+matteoklee@users.noreply.github.com>
2023-10-07 00:53:29 +0200

    initial setup

---
+++ src/test/java/de/kleemann/co2_hsharz/Co2HsHarzApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kleemann.co2_hsharz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2HsHarzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
