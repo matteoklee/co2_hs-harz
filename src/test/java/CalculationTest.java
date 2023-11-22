fe3f52b32f94c13151937551a9be2e301a6f4603
Matteo Kleemann <117347339+matteoklee@users.noreply.github.com>
2023-11-22 21:10:05 +0100

    junit tests fabian

--- src/test/java/CalculationTest.java
+++ src/test/java/CalculationTest.java
@@ -1,6 +1,13 @@
 import de.kleemann.co2_hsharz.persistence.transport.TransportMediumRepository;
+import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Class "CalculationTest" is used for ...
@@ -10,11 +17,24 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @since 22.11.2023
  */
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class CalculationTest {
 
-    TransportMediumRepository transportMediumRepos;
+    /*
+        Last Error: Unable to find a @SpringBootConfiguration, you need to use @ContextConfiguration or @SpringBootTest(classes=...) with your test
+
+     */
+    private final TransportMediumRepository transportMediumRepos;
+
+    public CalculationTest(final TransportMediumRepository testRepo){
+        this.transportMediumRepos = testRepo;
+    }
 
     @Test
     public void testFindFirstByTransportMediumNameAndTransportMediumSizeAndTransportMediumFuel(){
+
+        assertThat(1).isEqualTo(1); //TODO nur ein dummy, chill
     }
+
 }
