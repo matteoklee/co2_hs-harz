6b2c6855f93b1285252d3df127049ae6e5598c7c
Fabian Siemens <Fabian Siemens@Fabian-Desktop>
2024-03-26 02:19:03 +0100

    Restructured SecurityConfiguration. Now includes Token-Algorithm and Secret key and authenticationManager beans. Changed SecurityFilterChain.

--- src/main/java/de/kleemann/co2_hsharz/api/security/SecurityConfiguration.java
+++ src/main/java/de/kleemann/co2_hsharz/api/security/SecurityConfiguration.java
@@ -1,28 +1,30 @@
 package de.kleemann.co2_hsharz.api.security;
 
+import javax.crypto.SecretKey;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import de.kleemann.co2_hsharz.persistence.CustomRequestFilter;
+import de.kleemann.co2_hsharz.core.auth.JwtTokenFilter;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Class "SecurityConfiguration" is used for ...
@@ -35,7 +37,7 @@ import de.kleemann.co2_hsharz.persistence.CustomRequestFilter;
 @Configuration
 public class SecurityConfiguration implements WebMvcConfigurer {
 
-    AuthenticationManager authenticationManager;
+//    AuthenticationManager authenticationManager;
     private final HandlerInterceptor apiRequestInterceptor;
 
     @Value("${custom.user.username}")
@@ -50,10 +52,10 @@ public class SecurityConfiguration implements WebMvcConfigurer {
     @Value("${custom.admin.password}")
     private String adminPassword;
 
-    public SecurityConfiguration(HandlerInterceptor apiRequestInterceptor) {
+    public SecurityConfiguration(final HandlerInterceptor apiRequestInterceptor) {
         this.apiRequestInterceptor = apiRequestInterceptor;
     }
-
+    
     /**
      * Adds the {@link ApiRequestInterceptor} as a {@link HandlerInterceptor} to the {@link InterceptorRegistry}
      */
@@ -72,41 +74,46 @@ public class SecurityConfiguration implements WebMvcConfigurer {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
     
+    @Bean(name = "signatureAlgorithm")
+    MacAlgorithm signatureAlgorithm() {
+    	return Jwts.SIG.HS512;
+    }
     
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userDetailsService());
-        authenticationManager = authenticationManagerBuilder.build();
-
-        http.authorizeHttpRequests((authz)->authz
-                .requestMatchers("/auth/**").authenticated()
-                .anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults())
-                .authenticationManager(authenticationManager)
-                .sessionManagement(httpSecuritySessionManagementConfigurer ->
-                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
-        http.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.disable());
-        http.addFilterBefore(new CustomRequestFilter(), BasicAuthenticationFilter.class);
-        return http.build();
+    @DependsOn("signatureAlgorithm")
+    SecretKey secretKey(MacAlgorithm signatureAlgorithm) {
+    	return signatureAlgorithm.key().build();
     }
-
-
+    
     @Bean
-    UserDetailsService userDetailsService() {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        UserDetails user = User.withUsername(userUsername)
-                .password(passwordEncoder.encode(userPassword))
-                //.password("user")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withUsername(adminUsername)
-                .password(passwordEncoder.encode(adminPassword))
-                //.password("admin")
-                .roles("USER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    @DependsOn("userService")
+    AuthenticationManager authenticationManager(HttpSecurity http, UserDetailsService userService) throws Exception {
+    	AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+    	authenticationManagerBuilder.userDetailsService(userService);
+    	return authenticationManagerBuilder.build();
     }
-
+    
+    @Bean
+    @DependsOn("authenticationManager")
+    SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager, JwtTokenFilter jwtTokenFilter) throws Exception {   	
+    	http.csrf(customizer -> customizer.disable())	//Cross Site Request Forgery (CSRF) Protection is enabled by default. Disable only for developing purposes
+    		.cors(Customizer.withDefaults())	//Cross Origin Resource Sharing
+    		.sessionManagement(configurer -> {
+    			configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    		})
+    		.exceptionHandling(configurer -> {
+    			configurer.authenticationEntryPoint((request, response, exception) -> {
+    				response.sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.getMessage());
+    			});
+    		})
+    		.authenticationManager(authenticationManager)
+    		.authorizeHttpRequests(configurer -> {
+    			configurer
+    				.requestMatchers("/api/private/**").authenticated()
+    				.requestMatchers("/**").permitAll();
+    		})
+    		.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    	
+    	return http.build();
+    }
 }
