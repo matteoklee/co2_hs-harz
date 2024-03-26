fe480fd5f2af4f59e58bc8e74dd1f84fc9eb821e
Fabian Siemens <Fabian Siemens@Fabian-Desktop>
2024-03-26 02:23:30 +0100

    Reduced boilerplate code. Changed UserRole from String to enum

--- src/main/java/de/kleemann/co2_hsharz/persistence/auth/UserEntity.java
+++ src/main/java/de/kleemann/co2_hsharz/persistence/auth/UserEntity.java
@@ -1,9 +1,10 @@
 package de.kleemann.co2_hsharz.persistence.auth;
 
-import de.kleemann.co2_hsharz.core.auth.UserRole;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Class "UserEntity" is used for ...
@@ -12,7 +13,9 @@ import jakarta.persistence.Id;
  * @version 1.0
  * @since 16.10.2023
  */
+@Data
 @Entity
+@NoArgsConstructor
 public class UserEntity {
 
     @Id
@@ -20,46 +23,9 @@ public class UserEntity {
     private Long userId;
     private String userName;
     private String userPassword;
-    //@Enumerated(EnumType.STRING)
-    private String userRole;
-
-    public UserEntity() {
-
-    }
+    private UserRole userRole;
 
     public UserEntity(long userId) {
         setUserId(userId);
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-    public String getUserRole() {
-        return userRole;
-    }
-
-    public void setUserRole(String userRole) {
-        this.userRole = userRole;
     }
 }
